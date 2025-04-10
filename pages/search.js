bd20edbc7c377844871017b3f9ad1eb42d0172ed
smd995 <smd99540@gmail.com>
2025-04-10 17:52:02 +0900

    ProductList added

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,30 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import axios from "@/lib/axios";
 
 export default function Search() {
+  const [products, setProducts] = useState([]);
   const router = useRouter();
   const { q } = router.query;
+
+  async function getProducts() {
+    const res = await axios.get(`products/?q=${q}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
 
   return (
     <div>
       <h1>Search 페이지</h1>
       <SearchForm initialValue={q} />
       <h2>{q} 검색 결과</h2>
+      <ProductList products={products} />
     </div>
   );
 }
