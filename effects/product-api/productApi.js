aa91b7d93ce4d42b7286c1b789a520e8034472ae
smd995 <smd99540@gmail.com>
2025-04-10 15:55:44 +0900

    axios get test success

--- effects/product-api/productApi.js
+++ effects/product-api/productApi.js
@@ -4,3 +4,8 @@ export const getProduct = async (targetId) => {
   const res = await axios.get(`/products/${targetId}`);
   return res.data;
 };
+
+export const getAllProducts = async () => {
+  const res = await axios.get(`/products`);
+  return res.data;
+};
--- effects/product-api/productApi.spec.js
+++ effects/product-api/productApi.spec.js
@@ -1,9 +1,15 @@
 import { describe, test, expect } from "vitest";
-import { getProduct } from "./productApi";
+import { getAllProducts, getProduct } from "./productApi";
+import { productFixture } from "../../fixtures/product-fixture";
 
 describe("product-api", () => {
   test("getProduct", async () => {
     const data = await getProduct(1);
-    console.log(data);
+    expect(data).toEqual(productFixture.results[8]);
+  });
+
+  test("getAllProducts", async () => {
+    const data = await getAllProducts();
+    expect(data).toEqual(productFixture);
   });
 });
